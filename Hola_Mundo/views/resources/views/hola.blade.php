61d85d8c5235a589d830e87b7975d667c0dda20d
JOHNSL <johnasuarezl@gmail.com>
2019-05-23 12:41:50 -0500

    Hola mundo, views - creacion de una vista

---
+++ Hola_Mundo/views/resources/views/hola.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Hola Views</title>
+    </head>
+    <body>
+        <h1>Hola Mundo</h1>
+
+        Hola Mundo, realizado a partir de la creacion de una vista.
+    </body>
+</html>
